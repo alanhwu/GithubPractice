a2f8f4d8530ea382579c8c21dbc744dffa2989a0
Alan Wu <60207036+alanhwu@users.noreply.github.com>
2020-02-11 09:28:40 -0800

    Attempted to finish QueQue. Followed along on LinkedList class in class.

--- StackImplementation/src/LinkedList.java
+++ StackImplementation/src/LinkedList.java
@@ -1,7 +1,62 @@
 
-public class LinkedList {
-	private int size;
-	private int ref;
+public class LinkedList<T> { //<T> allows us to useand data type
+	private int size; //number of elements in list
+	private Node<T> head; //reference to front of the list
 	
 	
+	public LinkedList() {
+		//job of the contructor is to
+		//initialize the instance variables
+	}
+	
+	
+	public int size() {
+		return size;
+	}
+	
+	public Node peek() {
+		return head;
+	}
+	
+	//add a node at the end of the list
+	
+	public void Add(T t) {
+		T.add(t);
+		size++;
+		
+		if(head ==null) {
+			Node n = new Node(t);
+			head = n;
+		}
+		Node temp = head;
+		while(temp.next!=null) {
+			//we know it snot the end of hte list unles th enext node reference
+			// is pointing to null
+			temp = temp.next;
+		}
+		temp.next = new Node(t); //done!
+		
+		
+	}
+	
+	public void addFront(T t) {
+		
+	}
+	
+	public Node remove() {
+		return 
+	}
+	
+	
+	//remove Node at index i
+	//REQUIRES: i<size
+	public Node remove(int i) {
+		
+	}
+	
+	//ISCIRCULAR IS ON THE EXAM
+	
+	public peek() {
+		return 
+	}
 }
