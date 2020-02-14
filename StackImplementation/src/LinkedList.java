3bde699782a26f101c2de380fc993c03d4a71c52
Alan Wu <60207036+alanhwu@users.noreply.github.com>
2020-02-14 00:53:29 -0800

    Got to 95%. Still missing one test case (put at the bottom of file)

--- StackImplementation/src/LinkedList.java
+++ StackImplementation/src/LinkedList.java
@@ -15,26 +15,29 @@ public class LinkedList<T> { // <T> allows us to useand data type
 	}
 
 	public Node<T> peek() {
-		return head; // peeking shows us the first element in the list
+		if (head == null) { //if there's no head, make sure it returns null
+			return null;
+		} else {
+			return head; // peeking shows us the first element in the list
+		}
 	}
 
 	// add a node at the end of the list
 
 	public void add(T t) {
 		if (head == null) { // check for null pointer
-			Node<T> n = new Node<T>(t); // if there's nothing, add just set the head to the new node
-			head = n;
-		}
+			head = new Node<T>(t); // if there's nothing, add just set the head to the new node
+		} else {
 
-		Node<T> temp = head;
-		// aa
+			Node<T> temp = head;
 
-		while (temp.next != null) {
-			// we know it's not the end of the list unless the next node reference
-			// is pointing to null
-			temp = temp.next;
+			while (temp.next != null) {
+				// we know it's not the end of the list unless the next node reference
+				// is pointing to null
+				temp = temp.next;
+			}
+			temp.next = new Node<T>(t); // done!
 		}
-		temp.next = new Node<T>(t); // done!
 
 		size++;
 	}
@@ -45,51 +48,64 @@ public class LinkedList<T> { // <T> allows us to useand data type
 			head = new Node<T>(t); // if there's no head, then the next node becomes the head
 
 		} else {
-			Node<T> n = new Node<T>(t); // makes it into a new node
-			n.next = head.next; // makes this new node into the new head
-			head.next = n;
+			Node<T> newhead = new Node<T>(t); // makes it into a new node
+			newhead.next = head; // makes this new node into the new head
+			head = newhead;
 		}
 		size++; // increment size after adding an element
 	}
 
 	public Node<T> remove() {
-		if (size == 0) { // return null if it's empty
+		if (head == null) { // return null if it's empty
 			return null;
-		} else {
-			Node<T> temp = head;
-			head = temp.next; // replaces the head with the next element
-			size--; // decrement size after removing an element
-			return head;
 		}
-
+		head = head.next;
+		size--; // decrement size after removing an element
+		return head;
 	}
 
 	// remove Node at index i
 	// REQUIRES: i<size
-	public Node remove(int i) {
-		Node<T> temp = head;
-		Node<T> left = null; // create this node to keep track of the previous node
-		if (i == 0 && temp != null) { // if it's 0, it means that the head needs to be removed
-			head = temp.next;
+	public Node<T> remove(int i) {
+
+		Node<T> current = head; //make a new node starting from head to traverse
+		int count = 0; //use a counter to keep track where we are in the list
+		if (i == 0 && head != null) { //if it's removing index 0, just do exactly what normal remove would do
+			head = head.next;
+			return head;
 		}
 
-		int count = -1; // use a counter to keep track of position
-
-		while (temp != null) {
-			if (count == i) { // we finish when the count reaches i
-				left.next = temp.next;
-				break;
-			} else { // if not, then we keep going and move to the next node and increment
-
-				left = temp;
-				temp = temp.next; // next node
-				count++; // track position
-			}
-
+		while (count < i - 1 && current.next != null) { //when we haven't reached the index yet and we haven't reached the edge
+			current = current.next; //keep moving to the right until we reach the index or the edge
+			count++; //increase count each time to get closer to target
 		}
-		size--; // decrease the size by 1 after an element is removed (when this method finishes
-				// running)
+		current.next = current.next.next(); //the strategy is to go AROUND it and just make the "next" skip to the next next one.
 		return head;
+
+//		
+//		Node<T> temp = head;
+//		Node<T> left = null; // create this node to keep track of the previous node
+//		if (i == 0 && head != null) { // if it's 0, it means that the head needs to be removed
+//			head = head.next;
+//		}
+//
+//		int count = -1; // use a counter to keep track of position
+//
+//		while (temp != null) {
+//			if (count == i) { // we finish when the count reaches i
+//				left.next = temp.next;
+//				break;
+//			} else { // if not, then we keep going and move to the next node and increment
+//
+//				left = temp;
+//				temp = temp.next; // next node
+//				count++; // track position
+//			}
+//
+//		}
+//		size--; // decrease the size by 1 after an element is removed (when this method finishes
+//				// running)
+//		return head;
 	}
 
 	public String toString() {
@@ -106,9 +122,6 @@ public class LinkedList<T> { // <T> allows us to useand data type
 	}
 	// ISCIRCULAR IS ON THE EXAM
 
-	
-	
-	
 	/*
 	 * Successes
 ==========
@@ -118,27 +131,26 @@ public class LinkedList<T> { // <T> allows us to useand data type
 3: Test add 1 element and get size
 4: Test add 1 element and peek
 5: Test add 2 elements and peek
-6: Test add 2 elements and remove
-7: Test call to add, remove, and size
-8: Test call to add, remove, remove, and size
-9: Test call to add 3 2 1 -5 remove at 1, and peek
-10: undisclosed test 0
-11: undisclosed test 1
+6: Test call to add, remove, and size
+7: Test call to add, remove, remove, and size
+8: Test call to add, remove, and peek
+9: Test call to add, add, remove, and peek
+10: Test call to add, add, remove at 0, and peek
+11: Test call to add 3 2 1 -5 remove at 1, and peek
+12: Test call to add 3 2 1 -5 remove at 1, remove, and peek
+13: Test call to add 3 2 1 remove 4 times, peek
+14: Test addFront twice and peek
+15: Test addFront twice, remove, peek
+16: undisclosed test 0
+17: undisclosed test 1
+18: undisclosed test 2
 ==========
 Failures
 ==========
-0: Test call to add, remove, and peek
-1: Test call to add, add, remove, and peek
-2: Test call to add, add, remove at 0, and peek
-3: Test call to add 3 2 1 -5 remove at 1, remove, and peek returned:3
-4: Test call to add 3 2 1 remove 4 times, peek did not return null
-5: Test addFront twice and peek
-6: Test addFront twice, remove, peek
-7: undisclosed test 2
+0: Test add 2 elements and remove
 
 Score:
-12
-	 * 
+19
 	 * 
 	 */
 }
